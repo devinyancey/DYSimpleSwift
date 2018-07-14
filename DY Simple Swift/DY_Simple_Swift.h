a9d0497dbe65e90bab0f9f4d96c0eb7c585fde52
dayancey <dyancey38@gmail.com>
2018-07-14 07:49:25 -0400

    Created Framework

---
+++ DY Simple Swift/DY_Simple_Swift.h
@@ -0,0 +1,19 @@
+//
+//  DY_Simple_Swift.h
+//  DY Simple Swift
+//
+//  Created by Devin Yancey on 7/14/18.
+//  Copyright © 2018 Devin Yancey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DY_Simple_Swift.
+FOUNDATION_EXPORT double DY_Simple_SwiftVersionNumber;
+
+//! Project version string for DY_Simple_Swift.
+FOUNDATION_EXPORT const unsigned char DY_Simple_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DY_Simple_Swift/PublicHeader.h>
+
+
